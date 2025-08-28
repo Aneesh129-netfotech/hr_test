12b3ec91c55a2b62c5b3f3a87dbf622ccb2436b0
Aneesh129-netfotech <aneesh@netfotech.in>
2025-08-28 11:50:12 +0530

    Update server.js

--- mail-service/server.js
+++ mail-service/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['*'], // React dev servers
+  origin: "*", // React dev servers
   credentials: true
 }));
 
